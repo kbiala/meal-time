c3a992c4607eba181804bf22f64ec9f9d4fd8d44
thmyrk <przemyslav.piotrowski@gmail.com>
2016-08-12 16:33:07 +0200

    Serialize created_at attribute

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -2,4 +2,8 @@ class OrderSerializer < ActiveModel::Serializer
   attributes :id, :name
 
   has_many :meals
+
+  attribute :created_at do
+    object.created_at.strftime("%Y-%m-%d %H:%M")
+  end
 end
